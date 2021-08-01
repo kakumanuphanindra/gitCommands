0bffb4686779d1be7635d704562cc481c098d8ca
KAKUMANU PHANINDRA <39497783+kakumanuphanindra@users.noreply.github.com>
2021-08-01 15:03:13 +0500

    rebase with branch4 (#4)

--- v1.rb
+++ v1.rb
@@ -1,5 +1,8 @@
 class V1
 	def first_method
 		print "this is my first_method"
+        print "commit 2"
+        print "stash extra work branch3"
+        print "test after rebase to branch"
 	end
 end
